1cb35d7322608f22eef2fdcf0033d73bc15529e0
Cookiedata78 <119806667+Cookiedata78@users.noreply.github.com>
2023-06-15 09:07:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,56 @@
 __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
-
 import os
-import zipfile
 import shutil
+import zipfile
 
 def clean_cache():
-    try:
-        base_path=os.getcwd()
-        cache_dir=os.path.join(base_path,"files","cache")
-        if os.path.exists(cache_dir):
-            # If directory exists, delete contents
-            for filename in os.listdir(cache_dir):
-                file_path = os.path.join(cache_dir, filename)
-                try:
-                    if os.path.isfile(file_path) or os.path.islink(file_path):
-                        os.unlink(file_path)
-                    elif os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
-                except Exception as e:
-                    print('Failed to delete %s. Reason: %s' % (file_path, e))
-        else:
-            # If directory does not exist, create it
-            os.makedirs(cache_dir)
-            print('Cache directory created successfully')
-    except Exception as e:
-        print('An error occurred: %s' % e)
-def cache_zip(zip_path, cache_path):
-    clean_cache()
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(cache_path)
+    cache_dir = os.path.join(os.getcwd(), "cache")
+    
+    if os.path.exists(cache_dir):
+        shutil.rmtree(cache_dir)
+    
+    os.makedirs(cache_dir)
+    print("Cache cleaned.")
 
-import os
+def cache_zip(zip_path, cache_dir):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(cache_dir)
+    
+    print(f"Zip file '{zip_path}' extracted to cache directory.")
 
 def cached_files():
-    cached_files = []
-    cache_path = "/path/to/cache/folder"
-    # Get the absolute path to the 'files' directory
-    files_dir = os.path.abspath('files')
-    # Join the absolute 'files' path with the relative 'cache' path
-    cache_dir = os.path.join(files_dir, 'cache')
-    # Iterate through the files in the cache folder
-    for the_file in os.listdir(cache_dir):
-        # Add the absolute file path to the list
-        file_path = os.path.join(cache_dir, the_file)
-        cached_files.append(file_path)
-    return cached_files
-import re
+    cache_dir = os.path.join(os.getcwd(), "cache")
+    
+    files = []
+    for file_name in os.listdir(cache_dir):
+        file_path = os.path.join(cache_dir, file_name)
+        if os.path.isfile(file_path):
+            files.append(os.path.abspath(file_path))
+    
+    return files
 
 def find_password(file_paths):
-    password_pattern = re.compile(r'password:\s*(\S+)')
     for file_path in file_paths:
-        with open(file_path, 'r') as f:
-            content = f.read()
-            match = password_pattern.search(content)
-            if match:
-                password = match.group(1)
-                return password
-    return None
+        with open(file_path, 'r') as file:
+            content = file.read()
+            if "password" in content:
+                return "Password found in file: " + file_path
+    
+    return "Password not found in any files."
+
+# Testing the functions
+if __name__ == "__main__":
+    clean_cache()
+    
+    zip_file_path = "data.zip"
+    cache_directory = os.path.join(os.getcwd(), "cache")
+    cache_zip(zip_file_path, cache_directory)
+    
+    files = cached_files()
+    print("Cached Files:")
+    for file_path in files:
+        print(file_path)
+    
+    password = find_password(files)
+    print(password)
